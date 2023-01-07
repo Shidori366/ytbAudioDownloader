7ecbb6ec82fe981ad1b87e9a8bd1ca36392ced43
Shidori366 <d.m.shidori@gmail.com>
2023-01-08 00:43:37 +0100

    added progress, complete message, error message

--- ytbAudioDownloader.py
+++ ytbAudioDownloader.py
@@ -2,17 +2,30 @@ from pytube import YouTube
 from unidecode import unidecode
 import re
 
-def printInfo(target: YouTube) -> YouTube.streams:
-    print(target.author + " - " + target.title)
+fileSize = 0
+
+def printProgress(stream, chunk, bytes_remaining):
+    progress = 100 - ((bytes_remaining * 100) / fileSize)
+    formattedRemaining = "{:.2f}".format(progress) + '%'
+    print("Progress: " + formattedRemaining, end='\r')
+
+def completeMessage(stream, file_path):
+    print("Successfully downloaded at: ", file_path)
 
 def download(url: str):
-    targetToDownload = YouTube(url)
+    global fileSize
+    targetToDownload = YouTube(url, on_progress_callback=printProgress, on_complete_callback=completeMessage)
     print("Downloading audio from this target: ")
-    printInfo(targetToDownload)
+    print(targetToDownload.title)
     audioStream = targetToDownload.streams.get_audio_only()
+    fileSize = audioStream.filesize
     temp = unidecode(targetToDownload.title.replace(" ", "_"))
     desiredFileName = ''.join(re.findall(r"[a-zA-Z0-9_]", temp))
     audioStream.download(filename=desiredFileName + ".mp3")
 
-url = input("Enter url: ")
-download(url)
+while True: 
+    url = input("Enter url: ")
+    try: 
+        download(url)
+    except:
+        print("Couldn't download from this source")
